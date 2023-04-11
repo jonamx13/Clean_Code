13e6694d006501ed36f23a1c0cb5c268de68cafd
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-10 18:36:48 -0700

    Rule 1: Pronounceable and expressive names

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,4 +1,10 @@
-// const usage
-const PI = 3.14159;
-PI = 3.1416
-console.log(PI);
+//* Nomenclature rules
+//? 1- Pronounceable and expressive names
+    // Preferable in English and using camelCase
+    // Avoiding hyphens(-), underscores(_) and abbr.
+
+// Not recommended
+const yyyymmdstr = moment().format('YYYY/MM/DD');
+
+// Recommended
+const currentDate = moment().format('YYYY/MM/DD');
