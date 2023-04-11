a93342dc3777df2987f4cf48279121f001268846
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-10 18:27:34 -0700

    const usage

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,18 +1,4 @@
-//Prior to ES6
-var name = 'Jona';
-
-//Modern Javascript
-let name = 'Eduardo';
+// const usage
 const PI = 3.14159;
-
-//let usage
-let age = 28;
-
-{
-    let age = 29;
-    console.log('Value inside block', age); //> error, age doesn't exist here
-}
-
-console.log('Value outside block', age); //> 29
-age = age * 2;
-console.log('Changed Value', age); //> 58
+PI = 3.1416
+console.log(PI);
