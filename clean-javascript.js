e79828d5d4eac8818a8cd6a6909860edc730ec47
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-14 21:03:21 -0700

    Arrow functions and this

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,75 +1,35 @@
-//* Functions (parameters and arguments)
-//? Recommended to limit them up to 3 parameters
+//* Arrow Functions and 'this'
+const getResult = () => 'Results Jona';
 
-// Parameter: number
-function getDouble(number) {
-    return number * 2;
-}
+console.log(getResult());
 
-// Argument: 10
-getDouble(10); //> 20
+// Without arrow
+[1, 2, 3].map(function (n) {
+    return n * 2;
+});
 
-// Default parameter
-function greet(person = 'Strange') {
-    console.log(`Hello ${person}`);
-}
+// With arrow
+[1, 2, 3].map((n) => n * 2);
 
-greet(); //> Hello Strange
-greet('Jona'); //> Hello Jona
-
-/////
-//? This happens when we don't sent the argument
-function greet2(person) {
-    console.log(`Hello ${person}`);
-}
-
-greet2(); //> Hello undefined
-
-// Spread operator
-//? Rest must be the last parameter
-
-// Without rest(spread)
-function add(x,y) {
-    console.log(x + y);
-}
-
-add(1, 2, 3, 4, 5); //> 3 (it's ignoring the rest of arguments)
-
-//With rest(spread)
-function addSpread(x, y, ...args) {
-    console.log(args.reduce((prev, current) => prev + current, 0) + ( x + y ));
-}
-
-addSpread(1, 2, 3, 4, 5); //> 15
-
-// Using spread to copy elements
-const course = {
-    title: 'Definitive Javascript',
-    content: 'Anything you need to know',
-
-    upperCaseTitle: function(){
-        console.log(this.title.toUpperCase());
+// With 'this'
+const counterNormal = {
+    number: 0,
+    increase() {
+        setInterval(() => console.log(++this.number) , 1000) 
     }
 }
 
-const courseCloned = Object.assign({}, course);
-const spreadCourseCloned = { ...course};
+counterNormal.increase();
 
-console.log(spreadCourseCloned);
-spreadCourseCloned.upperCaseTitle();
+// Without 'this'
 
-// Cloning arrays with spreads
-const numbers = [1, 2 ,3];
+const counterThis = {
+    number: 0,
+    increase() {
+        setInterval(function() {
+            console.log(++this.number);
+        }.bind(this), 1000) // bind method
+    }
+}
 
-const clonedNumbers = numbers.slice();
-const spreadClonedNumbers = [...numbers];
-
-console.log(spreadClonedNumbers);
-
-// Linking two arrays with spread
-const numbersA = [1, 2, 3];
-const numbersB = [4, 5, 6];
-
-const newNumbers = [...numbersA, ...numbersB];
-
-console.log(newNumbers);
+counterThis.increase();
