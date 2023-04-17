a023692c19c702ba4e21ef831b2a9cac59eae806
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-16 20:41:59 -0700

    Reduced Size (Single-responsibility principle)

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,55 +1,43 @@
-//* Before ES6
-//? Parent Class
-function Person(name) {
-    this.name = name;
-}
+//* Reduced Size (Single-responsibility principle)
 
-Person.prototype.greet = function() {
-    console.log('Hello I am ' + this.name);
-}
-
-//? Child Class
-function Developer(name) {
-    this.name = name;
-}
-
-Developer.prototype = Object.create(Person.prototype);
-
-Developer.prototype.writeCode = function(coffee) {
-    if(coffee) console.log('I am working in a new feature');
-    else console.log('I need coffee, please!');
-}
-
-var dev = new Developer('Jona');
-dev.greet(); //> Hello I am Jona
-dev.writeCode(); //> I need coffee, please!
-
-
-//* With ES6
-//? Parent Class
-class Person2 {
-    constructor(name) {
-        this.name = name
+class UserAuth {
+    constructor(user) {
+        this.user = user;
     }
 
-    greet() {
-        console.log(`Hello I am ${this.name}`);
+    verifyCredentials() {
+        return false;
     }
 }
 
-//? Child Class
-class Developer2 extends Person2 {
-    constructor(name) {
-        super(name)
+class UserSettings extends UserAuth {
+    constructor(user, settings) {
+        super(user);
+        this.settings = settings;
     }
 
-    writeCode(coffee) {
-        coffee 
-        ? console.log('I am developing a new feature') 
-        : console.log('I need coffee, please!');
+    changeSetting(settings) {
+        if (this.verifyCredentials()) {
+            console.log('Can modify its settings');
+        } else {
+            console.log('It has not access');
+        }
     }
 }
 
-const dev2 = new Developer2('Candance');
-dev2.greet(); //> Hello I am Candance
-dev2.writeCode(); //> I need coffee, please
+const newAccess = new UserSettings('Jona', 'Dark Mode');
+newAccess.changeSetting();
+
+// class UserSettings {
+//     constructor(user, settings) {
+//         this.user = user;
+//         this.settings = settings;
+//     }
+
+//     changeSettings(settings) {
+//         if (this.verifyCredentials()) {
+//         }
+//     }
+
+//     verifyCredentials() {}
+// }
