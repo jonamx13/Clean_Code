273e6f9afa332077f8ab5e2f67cfecc445b03455
Jonathan Meixueiro <lav.jonathanom@gmail.com>
2023-04-16 20:29:14 -0700

    OOP with ES6 (Inheritance)

--- clean-javascript.js
+++ clean-javascript.js
@@ -1,28 +1,55 @@
 //* Before ES6
+//? Parent Class
 function Person(name) {
     this.name = name;
 }
 
 Person.prototype.greet = function() {
-    return 'Hello I am ' + this.name;
+    console.log('Hello I am ' + this.name);
 }
 
-var person = new Person('Jonathan');
-console.log(person.name);
-console.log(person.greet());
+//? Child Class
+function Developer(name) {
+    this.name = name;
+}
+
+Developer.prototype = Object.create(Person.prototype);
+
+Developer.prototype.writeCode = function(coffee) {
+    if(coffee) console.log('I am working in a new feature');
+    else console.log('I need coffee, please!');
+}
+
+var dev = new Developer('Jona');
+dev.greet(); //> Hello I am Jona
+dev.writeCode(); //> I need coffee, please!
 
 
 //* With ES6
-class Cat {
-    constructor(cat) {
-        this.name = cat;
+//? Parent Class
+class Person2 {
+    constructor(name) {
+        this.name = name
     }
 
     greet() {
-        return `Hello, I am a cat and my name is ${this.name}`
+        console.log(`Hello I am ${this.name}`);
     }
 }
 
-const cat = new Cat('Oliver')
-console.log(cat.name);
-console.log(cat.greet());
+//? Child Class
+class Developer2 extends Person2 {
+    constructor(name) {
+        super(name)
+    }
+
+    writeCode(coffee) {
+        coffee 
+        ? console.log('I am developing a new feature') 
+        : console.log('I need coffee, please!');
+    }
+}
+
+const dev2 = new Developer2('Candance');
+dev2.greet(); //> Hello I am Candance
+dev2.writeCode(); //> I need coffee, please
